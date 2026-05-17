a8a5c5a9ab0b4fce6db9c8aa4e3fb4d3dc3736bf
B251940009 <b251940009@erdenetis.edu.mn>
2026-05-17 11:14:43 +0800

    Add files via upload

---
+++ lab13/Main.java
@@ -0,0 +1,13 @@
+package lab13;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new DensityCalculator().setVisible(true);
+            }
+        });
+    }
+}
